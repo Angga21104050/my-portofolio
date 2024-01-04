77b8e3708caffa24516b9bc80753c69ca847816f
Angga21104050 <21104050@ittelkom-pwt.ac.id>
2024-01-04 20:37:06 +0700

    update darkmode + perbaikan fitur

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,11 +2,16 @@
 window.onscroll = function () {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
     if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 }
 
@@ -18,3 +23,33 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+
+// menutup humberger ketika diklik
+window.addEventListener('click', function (e) {
+    if (e.target !== hamburger && e.target !== navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+});
+
+// dark mode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', () => {
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    }
+    else {
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+// memindahkan posisi toggle sesuai dengan tema yang dipilih
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+  } else {
+    darkToggle.checked = false;
+  }
